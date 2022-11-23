72ac8309581e46a99ae0406db5c8de06936ba5d2
fuahyo <fuah566@gmail.com>
2022-11-23 19:10:05 +0700

    first

---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,23 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:freshly/src/blocs/blocs.dart';
+import 'package:freshly/src/cubits/cubits.dart';
+import 'package:freshly/src/models/models.dart';
+import 'package:freshly/src/utilities/utilities.dart';
+import 'package:freshly/src/widgets/widgets.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+part 'splash_screen.dart';
+part 'register_screen.dart';
+part 'login_screen.dart';
+part 'home/home_screen.dart';
+part 'home/views/dashboard_view.dart';
+part 'home/views/wishlist_view.dart';
+part 'home/views/user_view.dart';
+part 'admin_screen.dart';
+part 'detail_screen.dart';
+part 'cart_screen.dart';
